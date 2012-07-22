d760715cf75b452b3425da0dd2b1a3fe06eaefdc
Jonathan Rosser <jonathan.rosser@gmail.com>
2012-07-22 19:54:21 +0100

    fix capitalisation

--- TestIO.h
+++ TestIO.h
@@ -2,7 +2,7 @@
 #define TESTIO_H
 
 #include "globals.h"
-#include "config.h"
+#include "Config.h"
 
 #define HARDWAREVERSION 10  // 1.0 , version number needs to fit in byte (0~255) to be able to store it into config, this should only be changed by the manufacturer !
 
